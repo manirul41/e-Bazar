7c61cbfacc538f26c0c82fb0f7363feca4f3def6
Manirul Islam Tanim <manirul41@gmail.com>
2021-06-22 20:28:14 +0600

    fixed custom test field on submit data

--- src/components/Checkouts/CustomTextField.js
+++ src/components/Checkouts/CustomTextField.js
@@ -1,17 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Grid, TextField } from '@material-ui/core';
 import React from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
 
 const FormInput = ({ name, label, required }) => {
-    const { control } = useForm();
+    // const { control } = useForm();
     const isError = false;
 
     return (
         <Grid item xs={12} sm={6}>
             <Controller
                 name={name}
-                control={control}
+                // control={control}
                 error={isError}
                 render={({ field, fieldState: { error } }) => (
                     <TextField
@@ -21,15 +21,13 @@ const FormInput = ({ name, label, required }) => {
                         required={required}
                         fullWidth
                         id={name}
+                        name={name}
                         label={label}
                         autoComplete={name}
                         // autoFocus
                         error={!!error}
                     />
                 )}
-                rules={{
-                    required: 'Required',
-                }}
             />
         </Grid>
     );
